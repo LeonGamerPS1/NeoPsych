9d9c316836ed5daa40227fbb888b554be9acf2ce
sshmaster666 <fnfmaker666@gmail.com>
2025-05-18 18:58:18 +0200

    begin organizing

--- source/funkin/backend/Achievements.hx
+++ source/funkin/backend/Achievements.hx
@@ -1,3 +1,4 @@
+package funkin.backend;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
@@ -7,7 +8,7 @@ import flixel.group.FlxSpriteGroup;
 import flixel.util.FlxColor;
 import flixel.text.FlxText;
 
-using StringTools;
+
 
 class Achievements {
 	public static var achievementsStuff:Array<Dynamic> = [ //Name, Description, Achievement save tag, Hidden achievement
@@ -26,7 +27,8 @@ class Achievements {
 		["Hyperactive",					"Finish a Song without going Idle.",				'hype',					false],
 		["Just the Two of Us",			"Finish a Song pressing only two keys.",			'two_keys',				false],
 		["Toaster Gamer",				"Have you tried to run the game on a toaster?",		'toastie',				false],
-		["Debugger",					"Beat the \"Test\" Stage from the Chart Editor.",	'debugger',				 true]
+		["Debugger",					"Beat the \"Test\" Stage from the Chart Editor.",	'debugger',				 true],
+		['Erect on Mommy Mearest', 'Press C,U,M In order while playing milfy mommy bf mix', 'mommy',false]
 	];
 	public static var achievementsMap:Map<String, Bool> = new Map<String, Bool>();
 
--- source/import.hx
+++ source/import.hx
@@ -1,1 +1,9 @@
 import Paths;
+import funkin.backend.*;
+import funkin.backend.Achievements;
+
+import flixel.*;
+import flixel.graphics.FlxGraphic;
+import flixel.math.*;
+import flixel.graphics.frames.*;
+using StringTools;
